c5e4b4b88e73b197ddf4b609eec831f4d6d54ffb
dasundoloswala <tharindudasun0@gmail.com>
2023-10-01 08:55:28 +0530

    created the project

--- app/src/main/java/com/tbmr/dreamtravel/MainActivity.java
+++ app/src/main/java/com/tbmr/dreamtravel/MainActivity.java
@@ -1,14 +1,29 @@
 package com.tbmr.dreamtravel;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import com.airbnb.lottie.LottieAnimationView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
+
+    Context context;
+    LottieAnimationView animationView; // Declare as a member variable
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main); // Link to the XML layout
+
+        // Initialize your context and LottieAnimationView
+        context = this;
+        animationView = findViewById(R.id.logoLottieAnimation);
+
+        // Do something, for example, start the animation
+        animationView.playAnimation();
     }
 }
